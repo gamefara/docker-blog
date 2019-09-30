7a991188114d9e21597709831c6aa191a75b1ad7
game_Fara <game_fara@outlook.com>
2019-10-01 04:30:49 +0900

    modify access_container

--- access_container.sh
+++ access_container.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 if [ $# = 1 ]; then
-    if [ $1 = "wordpress" ]; then
+    if [ $1 = "w" ] || [ $1 = "wordpress" ]; then
         docker-compose exec wordpress bash
-    elif [ $1 = "mysql" ]; then
+    elif [ $1 = "m" ] || [ $1 = "mysql" ]; then
         . ./.env
-        docker-compose exec mysql bash -c "mysql -D ${MYSQL_DATABASE} -u${MYSQL_USER} -p${MYSQL_PASSWORD}"
+        docker-compose exec mysql bash -c "mysql -D ${_DB_NAME} -u${_DB_USER} -p${_DB_PASS}"
     fi
 else
     echo 'please "bash access_container.sh (container name)"'
