cb6f468f5f71bec07ec72761651fd6d35fc1c874
game_Fara <game_fara@outlook.com>
2019-10-11 23:51:17 +0900

    https access enable

--- create_container.sh
+++ create_container.sh
@@ -1,17 +1,9 @@
 #!/bin/sh
 
-bash ./rmi-container.sh
+docker ps -aq | xargs docker stop
 
 . ./.env
 sudo mkdir -p ${WORDPRESS_DIRECTORY}
 sudo mkdir -p ${MYSQL_DIRECTORY} 
-
-# create network
-LINK_NAME=blog_link
-NETWORK_COUNT=`docker network ls | grep ${LINK_NAME} | wc -l`
-if [ ${NETWORK_COUNT} = 1 ]; then
-    docker network rm ${LINK_NAME}
-fi
-sudo docker network create --driver bridge ${LINK_NAME}
 
 ./start_container.sh
