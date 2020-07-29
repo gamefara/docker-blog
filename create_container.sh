631ef0c7a187264d9cef991b829ff29f4c1bba4e
Fara <game_Fara@outlook.com>
2020-07-29 17:23:26 +0900

    npm installのエラーを修正

--- create_container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-docker ps -aq | xargs docker stop
-
-. ./.env
-sudo mkdir -p ${WORDPRESS_DIRECTORY}
-sudo mkdir -p ${MYSQL_DIRECTORY} 
-
-./start_container.sh
--- rmi_container.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-docker ps -aq | xargs docker stop | xargs docker rm
-docker image ls -aq | xargs docker rmi
